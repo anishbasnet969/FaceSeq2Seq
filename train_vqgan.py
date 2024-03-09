ce5a87d58c97d15ecc9780917ce64a4304c02a31
anishbasnet969 <anishbasnet969@gmail.com>
2024-03-09 23:16:32 +0545

    768z

--- train_vqgan.py
+++ train_vqgan.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--latent-dim",
         type=int,
-        default=1024,
+        default=768,
         help="Latent dimension n_z (for compatibility with roberta large hidden dim: 1024).",
     )
     parser.add_argument(
@@ -84,10 +84,10 @@ if __name__ == "__main__":
     gcs_log_dir = "gs://crossface-bucket/logs"
     gcs_ckpt_dir = "gs://crossface-bucket/checkpoints/vqgan"
 
-    logger = TensorBoardLogger("crossface-1024z-16b/vqgan/", name="logs")
+    logger = TensorBoardLogger("crossface-768z-16b/vqgan/", name="logs")
 
     checkpoint_callback = ModelCheckpoint(
-        dirpath="crossface-1024z-16b/vqgan",
+        dirpath="crossface-768z-16b/vqgan",
         filename="vqgan_epoch_{epoch:03d}",
         save_top_k=-1,
         every_n_epochs=50,
