0d85d590bc7f4927335d33ea346e44978fe7a828
anishbasnet969 <anishbasnet969@gmail.com>
2024-03-09 10:57:27 +0545

    scratch

--- train_vqgan.py
+++ train_vqgan.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--batch-size",
         type=int,
-        default=2,
+        default=4,
         help="Input batch size for training (default: 4)",
     )
     parser.add_argument(
@@ -84,10 +84,10 @@ if __name__ == "__main__":
     gcs_log_dir = "gs://crossface-bucket/logs"
     gcs_ckpt_dir = "gs://crossface-bucket/checkpoints/vqgan"
 
-    logger = TensorBoardLogger("crossface-1024z-8b/vqgan/", name="logs")
+    logger = TensorBoardLogger("crossface-1024z-16b/vqgan/", name="logs")
 
     checkpoint_callback = ModelCheckpoint(
-        dirpath="crossface-1024z-8b/vqgan",
+        dirpath="crossface-1024z-16b/vqgan",
         filename="vqgan_epoch_{epoch:03d}",
         save_top_k=-1,
         every_n_epochs=50,
